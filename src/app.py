b53fa74b8da1671c359605cf97dab7794fbc936f
Alejandro Balderas <alejandro@hotmail.de>
2021-03-10 08:44:58 +0100

    Animations dashboard from tutorials

--- src/app.py
+++ src/app.py
@@ -3,30 +3,22 @@ import dash_core_components as dcc
 import dash_html_components as html
 import plotly.express as px
 import pandas as pd
+import plotly.express as px
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
 
-# assume you have a "long-form" data frame
-# see https://plotly.com/python/px-arguments/ for more options
-df = pd.DataFrame({
-    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-    "Amount": [4, 1, 2, 2, 4, 5],
-    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
-})
+df = px.data.gapminder()
 
-fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+fig = px.scatter(df, x="gdpPercap", y="lifeExp", animation_frame="year", 
+        animation_group="country", size="pop", color="continent", 
+        hover_name="country", log_x=True, size_max=55, 
+        range_x=[100,100000], range_y=[25,90])
 
-app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
-
-    html.Div(children='''
-        Dash: A web application framework for Python.
-    '''),
-
+app.layout = html.Div([
     dcc.Graph(
-        id='example-graph',
+        id='life-exp-vs-gdp',
         figure=fig
     )
 ])
